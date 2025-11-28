e95f7869a6e2e4b5b928ee769e9dc5e1f73e14f3
CodeWithArafat1 <arafatnill2.0>
2025-11-28 15:01:54 +0600

    first commit

---
+++ L1/app.js
@@ -0,0 +1,5 @@
+var sub = function (n1, n2) {
+    return n1 + n2;
+};
+var ans = sub(10, 20);
+console.log(ans);
---
+++ L1/app.ts
@@ -0,0 +1,7 @@
+const sub = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+const ans = sub(10, 20);
+
+console.log(ans);
